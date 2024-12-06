ee77dfcd41193813a6a23d352c881559b14386f2
youda97 <ouda.yousef@gmail.com>
2024-12-06 04:26:51 -0500

    Rename folder

--- Frontend/src/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-// src/axiosInstance.ts
-import axios, { AxiosInstance } from 'axios';
-
-const firebaseProjectId = process.env.REACT_APP_FIREBASE_PROJECT_ID;
-const axiosInstance: AxiosInstance = axios.create({
-  baseURL: `https://${firebaseProjectId}.cloudfunctions.net/api/`
-});
-
-export default axiosInstance;
