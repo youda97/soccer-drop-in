6c9de7a5c7b927eb8e84f9ec91cecb24628a914b
youda97 <ouda.yousef@gmail.com>
2024-12-06 04:07:11 -0500

    Add capacitor

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.soccerdropin.app',
+  appName: 'soccer-drop-in',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
